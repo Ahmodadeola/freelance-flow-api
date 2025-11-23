a23ea1a75ffecd857f485a1406d380f03588ec89
Adeola Ahmad <ahmodadeola@gmail.com>
2025-11-23 19:33:14 +0100

    setup DB and prisma

---
+++ prisma/migrations/20251123180909_create_init_tables/migration.sql
@@ -0,0 +1,42 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "users";
+
+-- CreateEnum
+CREATE TYPE "users"."Role" AS ENUM ('FREELANCER', 'CLIENT', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users"."auth" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "auth_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users"."users" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "firstName" VARCHAR(50) NOT NULL,
+    "lastName" VARCHAR(50) NOT NULL,
+    "role" "users"."Role" NOT NULL DEFAULT 'FREELANCER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auth_email_key" ON "users"."auth"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auth_userId_key" ON "users"."auth"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"."users"("email");
+
+-- AddForeignKey
+ALTER TABLE "users"."auth" ADD CONSTRAINT "auth_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
