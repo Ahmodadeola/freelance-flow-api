93c725dbb9f5fa8c5e872e1896b19e3e37f23b70
Adeola Ahmad <ahmodadeola@gmail.com>
2025-12-24 16:52:29 +0100

    setup auth and user apps

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
+  }
+}
