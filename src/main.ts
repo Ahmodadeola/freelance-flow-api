5284d568caa380005edb25ba5e07a278a270eaed
Adeola Ahmad <ahmodadeola@gmail.com>
2025-12-24 16:27:11 +0100

    setup nest config and prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+
+  Logger.log(`🚀 Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
